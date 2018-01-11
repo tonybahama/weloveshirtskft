48d098667eeb630b1410a1be86b42966a0b62395
nemethgabor17 <nemethgabor17@gmail.com>
2018-01-11 10:05:06 +0100

    Create printer_user_role.php

---
+++ Snippets/printer_user_role.php
@@ -0,0 +1,15 @@
+// Add a custom user role
+
+$result = add_role( 'printer', __(
+
+'Printer' ),
+
+array(
+'edit_plugins' => true,
+'manage_options' => true,
+'unfiltered_html' => true,
+'manage_woocommerce' => true,
+'read' => true
+)
+
+);
